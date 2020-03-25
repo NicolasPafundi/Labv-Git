c7cc81de94c314a02d8fec3af2dbf1e8d7587de7
Nicolas Pafundi <nicolaspafundi95@gmail.com>
2020-03-25 01:32:35 -0300

    genericos

---
+++ TP_Genericos/src/Nodo.java
@@ -0,0 +1,33 @@
+public class Nodo<T>
+{
+    private T value;
+    private Nodo<T> next;
+
+    public Nodo(T value, Nodo<T> next)
+    {
+        this.value = value;
+        this.next = next;
+    }
+
+    public Nodo(T value)
+    {
+        this.value = value;
+        this.next=null;
+    }
+
+    public T getValue() {
+        return this.value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Nodo<T> getNext() {
+        return this.next;
+    }
+
+    public void setNext(Nodo<T> next) {
+        this.next = next;
+    }
+}
